4da5825fafcfa32916dfe47635ad475441c41672
Garrett Watson <garrettbauer@gmail.com>
2019-07-20 02:54:56 +0900

    Initial setup complete, services starting to render

--- server/server.js
+++ server/server.js
@@ -3,12 +3,33 @@ const proxy = require('express-http-proxy');
 const app = express();
 
 
-app.use('/:pid', express.static(`${__dirname}/public`));
+app.use('/:pid', express.static(`./public`));
 
-app.use('/api/product/:id', proxy('http://127.0.0.1:3003/api/product', {
+app.use('/api/photos/:id', proxy('http://127.0.0.1:3001', {
   proxyReqPathResolver: (req) => {
-    console.log(req);
-    return `http://127.0.0.1:3003/product/${req.url}`;
+    console.log(req.url);
+    return `/api/photos${req.url}`;
+  }
+}));
+
+app.use('/api/itemSummary/:id', proxy('http://127.0.0.1:3002', {
+  proxyReqPathResolver: (req) => {
+    console.log(req.url);
+    return `/api/itemSummary${req.url}`;
+  }
+}));
+
+app.use('/api/product/:id', proxy('http://127.0.0.1:3003', {
+  proxyReqPathResolver: (req) => {
+    console.log(req.url);
+    return `/api/product${req.url}`;
+  }
+}));
+
+app.use('/api/reviews/:id', proxy('http://127.0.0.1:3004', {
+  proxyReqPathResolver: (req) => {
+    console.log(req.url);
+    return `/api/reviews${req.url}`;
   }
 }));
 
